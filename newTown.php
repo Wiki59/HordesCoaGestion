52adcd1e6b0eddb3ad00529d7f886ed5d0add51c
Wiki59 <wiki59@live.fr>
2016-05-30 09:00:09 +0000

    newTown is working

--- newTown.php
+++ newTown.php
@@ -13,18 +13,21 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
             $_SESSION["token"] == $_POST["token"]
     ) {
         if (!is_dir("town/" . $_POST["town"])) {
-            mkdir("town/" . $_POST["town"]);
-            $file = "citizen.json";
+            $mk = mkdir("town/" . $_POST["town"]);
+		 $file = "citizen.json";
             $fd = fopen("town/" . $_POST["town"] . "/" . $file, "a+");
             $citizen = array();
             $citizen_json = json_encode($citizen);
-            fwrite($fd, $citizen_json);
-            fclose($fd);
-        } else {
+            $fw = fwrite($fd, $citizen_json);
+            $fc = fclose($fd);
+        	header("Location: accueil.php");
+	} else {
             echo "Nom de ville déjà existant";
+	 header("Location: accueil.php");
         }
     } else {
         header("HTTP/1.0 400 Bad Request", true, 400);
     }
+
 }
 
