727df748a56d29f62d1daff8ff52d21cac64a8c9
Wissam Lefevre <wiki59@live.fr>
2016-05-30 14:12:18 +0200

    rangement

--- entete.php
+++ entete.php
@@ -1,6 +1,6 @@
 <div id="entete">
-    <label id="mainLabel"></label>
-    <input type="text" placeholder="Voir ville" id="showTown" class="searcher" plcHoldI="0"/>
+    <label id="mainLabel">Deconnecté</label>
+    <input type="text" id="showTown" class="searcher" plcHoldI="0"/>
     <form id="formTown" action="newTown.php" method="POST">
         <input id="token" name="token" type="hidden" value="<?php echo $token; ?>"/>
         <input name="town" type="text" placeholder="Créer une ville"/>
--- searcher.js
+++ searcher.js
@@ -10,58 +10,58 @@ $(".searcher").on("focus", function () {
         type: "POST",
         url: "allTown.php?token=" + $("#token").val(),
         success: function (data) {
-		var maper = [];
+            var maper = [];
             JSON.parse(data, function (k, v) {
-			maper.push(v);
-		});
-		// Affichage en autocomplete
-                target.autocomplete({
-                    source: maper,
-                    autoFocus: true,
-                    delay: 50,
-                    response: function (e, map) {
-                        resultTab = [];
-                        $.each(map, function (k, v) {
-                            $.each(v, function (key, val) {
-                                resultTab.push(val);
-                            });
+                maper.push(v);
+            });
+            // Affichage en autocomplete
+            target.autocomplete({
+                source: maper,
+                autoFocus: true,
+                delay: 50,
+                response: function (e, map) {
+                    resultTab = [];
+                    $.each(map, function (k, v) {
+                        $.each(v, function (key, val) {
+                            resultTab.push(val);
                         });
-                        resultShow();
+                    });
+                    resultShow();
+                }
+            });
+            // Affichage en placeholder
+            if (maper.length > 0) {
+                target.attr("placeholder", maper[parseInt(target.attr("plcHoldI"))].value);
+                // Le keydown
+                target.on("keydown.holdering", function (e) {
+                    actual = parseInt(target.attr("plcHoldI"));
+                    if (e.keyCode === 38 && actual - 1 >= 0) {
+                        target.attr("plcHoldI", parseInt(actual) - 1);
+                        target.attr("placeholder", maper[parseInt(target.attr("plcHoldI"))].value);
+                    } else if (e.keyCode === 40 && actual + 1 < maper.length) {
+                        target.attr("plcHoldI", actual + 1);
+                        target.attr("placeholder", maper[parseInt(target.attr("plcHoldI"))].value);
+                    } else if (e.keyCode === 39 || e.type === "dblclick") {
+                        target.val(target.attr("placeHolder"));
                     }
                 });
-                // Affichage en placeholder
-                if (maper.length > 0) {
-                    target.attr("placeholder", maper[parseInt(target.attr("plcHoldI"))].value);
-                    // Le keydown
-                    target.on("keydown.holdering", function (e) {
-                        actual = parseInt(target.attr("plcHoldI"));
-                        if (e.keyCode === 38 && actual - 1 >= 0) {
-                            target.attr("plcHoldI", parseInt(actual) - 1);
-                            target.attr("placeholder", maper[parseInt(target.attr("plcHoldI"))].value);
-                        } else if (e.keyCode === 40 && actual + 1 < maper.length) {
-                            target.attr("plcHoldI", actual + 1);
-                            target.attr("placeholder", maper[parseInt(target.attr("plcHoldI"))].value);
-                        } else if (e.keyCode === 39 || e.type === "dblclick") {
-                            target.val(target.attr("placeHolder"));
-                        }
-                    });
-                } else {
-                    target.attr("placeholder", "...");
-                }
-                // Affichage liste, dans une div#result
-                target.on("keydown.resulter", resultShow = function () {
-                    $("#result").html("<h3>Resultat :</h3>");
-                    $.each(resultTab, function (key, val) {
-                        $("#result").append("<a>" + val.value + "</a>");
-                        $("#result").append("<br/>");
-                    });
+            } else {
+                target.attr("placeholder", "...");
+            }
+            // Affichage liste, dans une div#result
+            target.on("keydown.resulter", resultShow = function () {
+                $("#result").html("<h3>Resultat :</h3>");
+                $.each(resultTab, function (key, val) {
+                    $("#result").append("<a>" + val.value + "</a>");
+                    $("#result").append("<br/>");
                 });
-                target.one("focus", resultShow());
-                 // On retire les keydown, si non il se cumule
-                target.on("blur", function () {
-                    target.attr("plcHoldI", "0");
-                    target.off("keydown.holdering keydown.resulter");
-                });
+            });
+            target.one("focus", resultShow());
+            // On retire les keydown, si non il se cumule
+            target.on("blur", function () {
+                target.attr("plcHoldI", "0");
+                target.off("keydown.holdering keydown.resulter");
+            });
         }
     });
 });
