709dd19a8fb441526c79abc520fec27d7579fc33
Wissam Lefevre <wiki59@live.fr>
2016-05-30 10:48:22 +0200

    form newTown in index plus style sheet

--- accueil.php
+++ accueil.php
@@ -1,20 +1,34 @@
+<?php
+session_start();
+$token = uniqid(rand(), true);
+$_SESSION['token'] = $token;
+$_SESSION['token_time'] = time();
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta http-equiv="Content-Language" content="fr"/>
-	<link rel="icon" href="allumette.ico" />
-	<title>
-  		Manager
-	</title>
-	<script type="text/javascript" src="jquery22.js"></script>
-	<script type="text/javascript">
-		code = "<?php echo $_GET["code"]; ?>";
-	</script>
-</head>
-<body>
-	<div id="content">
-	</div>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="Content-Language" content="fr"/>
+        <link rel="stylesheet" href="style.css" />
+        <link rel="icon" href="allumette.ico" />
+        <title>
+            Manager
+        </title>
+        <script type="text/javascript" src="jquery22.js"></script>
+        <script type="text/javascript">
+            code = "<?php echo $_GET["code"]; ?>";
+        </script>
+    </head>
+    <body>
+        <div id="entete">
+            <form class="tokenSubmitter" action="newTown.php" method="POST">
+                <input name="token" type="hidden" value="<?php echo $token; ?>"/>
+                <input name="town" type="text" placeholder="Créer une ville"/>
+                <input type="submit" value="Go"/>
+            </form>
+        </div>
+        <div id="content">
+        </div>
         <script type="text/javascript" src="script.js"></script>
-</body>
+    </body>
 </html>
