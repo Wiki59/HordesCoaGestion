194dd24170cbdcdd844a60ca33a34ec2c5e3d70d
Wiki59 <wiki59@live.fr>
2016-05-31 11:53:50 +0000

    requete mid end new citizen

--- newCitizen.php
+++ newCitizen.php
@@ -1,25 +1,36 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     header("HTTP/1.0 405 Method Not Allowed", true, 405);
 } else {
-    $citizens = file_get_contents("/town/" . "town1" . "/citizen.json");
-    $array_citizen = json_decode($citizens);
-    echo var_dump($array_citizen);
+/*
+    $citizens = file_get_contents("town/" . "town1" . "/citizen.json");
+    $array_citizen = json_decode($citizens, true);
+    //echo var_dump($array_citizen['citizen']);
 
-
-    /*
+	$array_citizen['citizen']['melou'] = array(
+		"heros" => true,
+	);
+	echo var_dump($array_citizen['citizen']['melou']);*/
       session_start();
-      if (isset($_SESSION["token"]) && isset($_POST["token"]) && $_SESSION["token"] == $_POST["token"] && $_SESSION["token_time"] >= (time() - (10 * 60) && isset($_POST["town"]) && isset($_POST["nom"]))) {
-      if (is_dir("/town/" . $_POST["town"])) {
-      $citizens = file_get_contents("/town/" . $_POST["town"] . "/citizen.json");
-      $array_citizen = json_decode($citizens);
-      } else {
-      echo "Citoyen déjà présent";
-      }
-      } else {
-      header("HTTP/1.0 400 Bad Request", true, 400);
-      }
-     */
+      	if (isset($_SESSION["token"]) && isset($_POST["token"]) && $_SESSION["token"] == $_POST["token"] && isset($_POST["town"]) && isset($_SESSION["user"]))) {
+      		if (is_dir("/town/" . $_POST["town"])) {
+      			$citizens = file_get_contents("/town/" . $_POST["town"] . "/citizen.json");
+      			$array_citizen = json_decode($citizens);
+			$pseudo = $_SESSION['user']['pseudo'];
+			if (array_key_exists($pseudo, $array_citizen)) {
+				echo "Citoyen déjà dans la liste";
+			} else {
+				$array_citizen['citizen'][$pseudo] = array(
+					"job" =>
+                			"heros" => true,
+					
+        			);
+			}
+      		} else {
+      			echo "Ville introuvable";
+      		}
+      	} else {
+      		header("HTTP/1.0 400 Bad Request", true, 400);
+      	}
 }
-
