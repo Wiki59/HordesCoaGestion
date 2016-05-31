d13afffb37e0625e29e86d03e8f4dd3a20d51e49
Wissam Lefevre <wiki59@live.fr>
2016-05-31 15:46:24 +0200

    script op first try

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ var user;
 $.get("info.php?state=Ok&code=" + code, function (data) {
     user = JSON.parse(data);
     if (user['pseudo'] != undefined) {
-	$("#mainLabel").html(user['pseudo']);
-	}
+        $("#mainLabel").html(user['pseudo']);
+    }
 });
 
 function addMe(town) {
-
+    $.post("/newCitizen.php?town=" + town, function (data) {
+        $("#boxInfo").html(data);
+    });
 }
 
 function majMe(town) {
-
+    $.post("/majCitizen.php?town=" + town, function (data) {
+        $("#boxInfo").html(data);
+    });
 }
