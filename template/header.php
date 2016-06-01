b73cc2738eb1e8d134346ef30927916690f8f3a5
Wissam Lefevre <wiki59@live.fr>
2016-06-01 16:25:44 +0200

    header missing

---
+++ template/header.php
@@ -0,0 +1,15 @@
+<head>
+    <title>
+        Manager
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Language" content="fr"/>
+    <link rel="icon" href="../ressource/allumette.ico" />
+    <link rel="stylesheet" href="../style/style.css" />
+    <link rel="stylesheet" href="../script/jquery-ui-1114/jquery-ui.min.css"/>
+    <script type="text/javascript" src="../script/jquery.js"></script>
+    <script type="text/javascript" src="../script/jquery-ui.js"></script>
+    <script type="text/javascript">
+        var code = "<?php echo $_GET['code']; ?>";
+    </script>
+</head>
