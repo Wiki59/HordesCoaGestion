e606ab35f0545cd023ad195cc1942bb3ac08431c
Wissam Lefevre <wiki59@live.fr>
2016-06-03 11:36:07 +0200

    all picture added

--- town.php
+++ town.php
@@ -23,23 +23,23 @@
 			<th>DP</th>
 			<th>Job</th>
 			<th><img src='ressource/h_guard.gif' alt='Point de défense' width='16' height='16'></th>
-			<th>Niveau ruines</th>
-			<th>Armageddon </th>
+			<th><img src='ressource/r_ruine.gif' alt='Niveau d'expertise pour les ruines' width='16' height='16'></th>
+			<th><img src='ressource/arma.gif' alt='Témoins de l'armageddon width='16' height='16'></th>
 			<th>G. Contamination</th>
-			<th>APAG </th>
-			<th>Sauvetage </th>
-			<th>RDH </th>
-			<th>Uppercut sauvage</th>
-			<th>Camaraderie </th>
-			<th>Second souffle</th>
-			<th>Nv. Trouvaille</th>
-			<th>Trompe la mort</th>
-			<th>Campeur pro</th>
+			<th><img src='ressource/apag.gif' alt='Appareil Photo de l'Avant Guerre width='16' height='16'></th>
+			<th><img src='ressource/h_calim.gif' alt='Sauvetage' width='16' height='16'></th>
+			<th>RDH</th>
+			<th><img src='ressource/small_wrestle.gif' alt='Uppercut sauvage' width='16' height='16'></th>
+			<th><img src='ressource/r_share.gif' alt='Camaraderie' width='16' height='16'></th>
+			<th><img src='ressource/small_pa.gif' alt='Second souffle' width='16' height='16'></th>
+			<th><img src='ressource/item_chest_hero.gif' alt='Niveau de trouvaille (Débrouillardise, Jolie trouvaille, Prévoyant, Avantage ancien joueur)' width='16' height='16'></th>
+			<th><img src='ressource/h_death.gif' alt='Trompe la mort' width='16' height='16'></th>
+			<th><img src='ressource/r_cmplst.gif' alt='Campeur pro' width='16' height='16'></th>
 			<th>Veilleur pro</th>
-			<th>Bann </th>
-			<th>Goule </th>
+			<th><img src='ressource/h_ban.gif' alt='Volontaire si besoin d'un banni' width='16' height='16'></th>
+			<th><img src='ressource/small_ghoul.gif' alt='Volontaire si besoin d'une goule width='16' height='16'></th>
 			<th>JH restant</th>
-			<th>Rôle </th>
+			<th>Rôle</th>
 			<th>Moyen de contact</tr>";
                 foreach ($array['citizen'] as $pseudo => $citizen) {
                     echo "<tr><td>$pseudo</td></tr>";
