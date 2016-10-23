25ba9b4f43d715e0d421c775b479b7bad5b26629
Wiss <wiki59@live.fr>
2016-10-23 19:18:36 +0200

    minor edit

--- template/entete.php
+++ template/entete.php
@@ -9,11 +9,11 @@ $_SESSION['token'] = $token;
 // Un délai est fixé au token, la vérification se fait dans les pages de requêtes
 $_SESSION['token_time'] = time();
 ?>
+<a id="recoLink" href="requete/logout.php"><img id="rope" src="../ressource/rope.png"/></a>
 <div id="entete">
     <div id="userDiv">
-	<a href="me.php" id="mainLabel"><?php echo $_SESSION["user"]["pseudo"]?></a>
-	<script>var pseudo = "<?php echo $_SESSION["user"]["pseudo"]  ?>"</script>
-    	<a id="recoLink" href="requete/logout.php"><img id="rope" src="../ressource/rope.png"/></a>
+        <a href="me.php" id="mainLabel"><?php echo $_SESSION["user"]["pseudo"] ?></a>
+        <script>var pseudo = "<?php echo $_SESSION["user"]["pseudo"]  ?>"</script>
     </div>
     <input type="text" placeholder="Ville" id="showTown" class="searcher" plcHoldI="0"/>
     <form id="formTown" action="requete/newTown.php" method="POST">
