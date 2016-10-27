6886a6b6f94dfc7ed9030dd5d80300be35125ced
Wiss <wiki59@live.fr>
2016-10-27 06:43:32 +0200

    All town in accueil

--- template/recentTown.php
+++ template/recentTown.php
@@ -1,7 +1,9 @@
 <div id="recentTownList">
     <?php
-    $dir = dir("../town");
-    var_dump($dir);
+    $dir = dir("../town/");
+    var_dump(is_dir($dir));
+    var_dump(is_dir("."));
+    var_dump(is_dir(".."));
     $towns = array();
     while (false !== ($t = $dir->re²ad())) {
         if (is_dir($t) && $t != "." && $t != "..") {
