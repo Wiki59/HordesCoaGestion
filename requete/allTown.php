9f8dd9ee75d6c0ce50e8a63dc21910a554a31f50
Wiss <wiki59@live.fr>
2016-10-27 07:39:30 +0200

    allTown.php is op

--- requete/allTown.php
+++ requete/allTown.php
@@ -1,16 +1,13 @@
 <?php
 
-//if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-//    header("HTTP/1.0 405 Method Not Allowed", true, 405);
-//} else {
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header("HTTP/1.0 405 Method Not Allowed", true, 405);
+} else {
     session_start();
     if (
-    /* A remettre
-      isset($_SESSION["token"]) &&
-      isset($_POST["token"]) &&
-      $_SESSION["token"] == $_POST["token"]
-     */
-            true
+        isset($_SESSION["token"]) &&
+        isset($_POST["token"]) &&
+        $_SESSION["token"] == $_POST["token"]
     ) {
         $dir = scandir("../town");
         $towns = array();
@@ -22,5 +19,5 @@
         echo json_encode($towns);
     } else {
         header("HTTP/1.0 400 Bad Request Token", true, 400);
-//    }
+    }
 }
