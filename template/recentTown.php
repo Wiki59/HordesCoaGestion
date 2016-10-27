3cf59dbbe2b6c344016b03d1a11a145445e6320e
Wiss <wiki59@live.fr>
2016-10-27 06:49:34 +0200

    All town in accueil

--- template/recentTown.php
+++ template/recentTown.php
@@ -1,15 +1,11 @@
 <div id="recentTownList">
     <?php
-    $dir = dir(".");
-    var_dump(is_dir($dir));
-    if (is_dir($dir)) {
-        while (false !== ($t = $dir->read())) {
-            if (is_dir($t) && $t != "." && $t != "..") {
-                echo "<li>" . var_dump($t) . "</li>";
-            }
+    $dir = scandir(".");
+    var_dump($dir);
+    foreach ($dir as $t) {
+        if (is_dir($t) && $t != "." && $t != "..") {
+            echo "<li>" . var_dump($t) . "</li>";
         }
     }
-    $dir->close();
-    echo "<p>Fin</p>";
     ?>
 </div>
