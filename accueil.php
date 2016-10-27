a6683e59ca4b979adf4e349f92e16d8e2dd410a3
Wiss <wiki59@live.fr>
2016-10-27 06:59:09 +0200

    All town in accueil

--- accueil.php
+++ accueil.php
@@ -13,7 +13,7 @@
         var_dump($dir);
         foreach ($dir as $t) {
             if (is_dir($t) && $t != "." && $t != "..") {
-                echo "<li>" . var_dump($t) . "</li>";
+                echo "<li>" . $t . "</li>";
             }
         }
         ?>
